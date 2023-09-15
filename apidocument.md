f2284ec94d44be9e644a320ddd4bd56e6c05fe63
Sid-Dhawan-technocrat <sdhawan127@gmail.com>
2023-09-15 17:49:33 +0530

    filter and sorting done

--- apidocument.md
+++ apidocument.md
@@ -12,3 +12,30 @@ List of restaurants
 
 http://localhost:4030/restaurants
 
+restaurant wrt city
+
+http://localhost:5000/restaurants?state_id=1
+
+## Page 2
+restaurant wrt meal type
+http://localhost:5000/restaurants?mealId=1
+
+restaurant wrt cuisine & meal
+http://localhost:5000/filter/1?cuisineId=2
+
+restaurant wrt cost & meal
+http://localhost:4030/filter/1?lcost=800&hcost=1000
+
+sort on the basis of cost
+
+## Page 3
+
+Details of a restaurant
+
+menu of restaurant
+
+## Page 4 
+
+Menu Details
+
+PlaceOrder
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT=4030
 const mongo = require("mongodb")
 const MongoClient=mongo.MongoClient
 const MONGO_URL = "mongodb://127.0.0.1:27017";
+app.use(express.json());
 let db;
 
 app.get("/",function(req,res){
@@ -15,7 +16,7 @@ app.get("/userList",function(req,res){
 })
 
 app.get("/locations",function(req,res){
-    const result=db.collection("location").find().toArray((err,result)=>{
+    const result=db.collection("locations").find().toArray((err,result)=>{
         if(err) throw err;
         res.send(result);
     });
@@ -28,13 +29,27 @@ app.get("/quickSearch",function(req,res){
     });
 })
 
-
+//get restaurant data by stateId || mealId
 app.get("/restaurants",function(req,res){
-    const result=db.collection("restaurants").find().toArray((err,result)=>{
+    let query={};   
+    let stateId=Number(req.query.state_id);
+    let mealId=Number(req.query.mealId);
+    if(stateId){
+        query={state_id:stateId};
+    }
+    else if(mealId){
+        query={"mealTypes.mealtype_id":mealId};
+    }
+    
+    db.collection("restaurants")
+    .find(query)
+    .toArray((err,result)=>{
         if(err) throw err;
         res.send(result);
     });
-})
+});
+
+
 // app.get("/filter/:mealId",function(req,res){
 //     let query={};
 //     let mealId=Number(req.params.mealdId);
@@ -51,7 +66,55 @@ app.get("/restaurants",function(req,res){
 
 // })
 
+//filter(Cuisine and cost filter)
+app.get("/filter/:mealId",function(req,res){
+    let query={};   
+    let mealId=Number(req.params.mealId);
+    let cuisineId=Number(req.query.cuisineId);
+    let lcost=Number(req.query.lcost);
+    let hcost=Number(req.query.hcost);
+    let sort={cost:1};
+    if(req.query.sort){
+        sort ={cost:req.query.sort}
+    }
+    if(cuisineId){
+        query={
+            "mealTypes.mealtype_id":mealId,
+            "cuisines.cuisine_id":cuisineId,
+        };
+    }else if(lcost && hcost){
+        query={
+            "mealTypes.mealtype_id":mealId,
+            $and:[{cost:{$gt:lcost,$lt:hcost}}],
+        }
+    }
+    else if(cuisineId && lcost && hcost){
+        query={
+            "mealTypes.mealtype_id":mealId,
+            "cuisines.cuisine_id":cuisineId,
+            $and:[{cost:{$gt:lcost,$lt:hcost}}],
+
+        }
+    }
+
+    db.collection("restaurants")
+    .find(query)
+    .sort(sort)
+    .toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result);
+    });
+});
+
+
+
+
+
+
 //MongoDB connection
+
+
+
 
 MongoClient.connect(MONGO_URL,(err,client)=>{
     console.log("Mongodb is connected");
